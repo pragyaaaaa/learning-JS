ff0861dac8856d2f58bd1038980b36f8ed075018
pragyaaaaa <pragyasabstracts@outlook.com>
2020-07-18 21:17:09 +0530

    arrow functions comments added.

--- CollectionSecTen/ArrowFunctions.js
+++ CollectionSecTen/ArrowFunctions.js
@@ -1,6 +1,18 @@
-/*syntactically compact alternative to a regular function expression
+/*Syntactically compact alternative to a regular 
+function expression (i.e. defining function inside an expression).
 They behave just like function expressions, but we have options regarding syntax
-depending on number of params.*/
+depending on number of params.
+1. Name can be omitted in function expressions to create anonymous functions.
+2. Function expressions are NOT hoisted.
+3. Arrow functions don't have their own 'this' value. The value of this inside an 
+arrow function is always inherited from the enclosing scope.
+4. Arrow functions can't be used as constructors.
+5. Arrow functions don't have prototype property.
+6. Arrow functions don't have their arguments object (rest parameters can be a good alternative).
+
+A very nice article to read: https://hacks.mozilla.org/2015/06/es6-in-depth-arrow-functions/
+*/
+
 function arrowFunction() {
     //parens are optional with one param
     const square = x => {
